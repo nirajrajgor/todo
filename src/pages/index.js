311373d379decdb3f75dbbf5f7eabe5a37e7c8db
nirajrajgor <nirajr1992@gmail.com>
2020-04-20 00:57:22 +0530

    Basic todo application with add and delete function ready.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,70 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends Component {
+
+	state = {
+		taskName: ""
+	}
+
+	onChangeValue = (e) => {
+		this.setState({ taskName: e.target.value });
+	};
+
+
+	onSubmitMethod = (e) => {
+		e.preventDefault();
+		let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+		let task = {
+			id: (tasks.length > 0 ? tasks[tasks.length - 1].id : 0) + 1,
+			text: this.state.taskName,
+			complete: false
+		};
+		tasks.push(task);
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+		this.setState({ taskName: "" });
+	}
+
+	deleteTask = (id) => {
+		let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+		tasks = tasks.filter(task => task.id !== id);
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+		this.forceUpdate();
+	}
+
+	render() {
+		return (
+			<Layout>
+				<SEO title="Home" />
+				<h1>Todos</h1>
+				<form onSubmit={this.onSubmitMethod}>
+					<input type="text" value={this.state.taskName} onChange={this.onChangeValue} />
+					<button type="submit">Submit</button>
+				</form>
+				{
+					localStorage.getItem('tasks') ?
+						JSON.parse(localStorage.getItem('tasks')).map((task, i) => (
+							<>
+								<span key={i}>{task.text}</span>{' '}
+								<button onClick={() => this.deleteTask(task.id)}>Delete</button>
+								<br />
+							</>
+						))
+						:
+						<p>Nothing to do! Add a task?</p>
+
+				}
+				<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+					<Image />
+				</div>
+				<Link to="/page-2/">Go to page 2</Link>
+			</Layout>
+		)
+	}
+}
 
 export default IndexPage
